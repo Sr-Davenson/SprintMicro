71de451964b674b9df144e9f116f8f8c083f1fe3
unknown <dsgil@uniboyaca.edu.co>
2025-05-27 21:17:50 -0500

    David: se arreglo funcion en controller

--- retro_items/routes/api.php
+++ retro_items/routes/api.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(RetroController::class)->group(function(){
-    Route::get('retro_items','index');
-    Route::post('retro_item','store');
-    Route::put('retro_item/{id}','update');
-    Route::delete('retro_item/{id}','destroy');
+    Route::get('retro','index');
+    Route::post('retro','store');
+    Route::put('retro/{id}','update');
+    Route::delete('retro/{id}','destroy');
 });
 
 Route::controller(SprintController::class)->group(function(){
